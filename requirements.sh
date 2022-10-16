967148034fe54b4ba72eedceca820347e41d9c25
jivnesh <jivnesh@pop-os.localdomain>
2022-10-16 17:16:28 +0530

    minor changes

--- requirements.sh
+++ requirements.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-PATH_TO_FASTNLP="$1"
+PATH_TO_ENVS="$1"
 
 conda env create -f tlat0.yml
 
 conda activate tlat0
 
-cp fastnlp-copy/core/tester.py $PATH_TO_FASTNLP/core/
+cp fastnlp-copy/core/tester.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/
 
-cp fastnlp-copy/core/dataset.py $PATH_TO_FASTNLP/core/
+cp fastnlp-copy/core/dataset.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/
 
-cp fastnlp-copy/core/metric.py $PATH_TO_FASTNLP/core/
+cp fastnlp-copy/core/metric.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/
 
-cp fastnlp-copy/modules/decoder/crf.py $PATH_TO_FASTNLP/modules/decoder/
+cp fastnlp-copy/modules/decoder/crf.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/modules/decoder/
 
 
--- train_sighum_shr.sh
+++ train_sighum_shr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PATH_TO_FASTNLP="$1"
+PATH_TO_ENVS="$1"
 st='sighum-shr'
 
 cd sktWS
 cp SIGHUM/* .
 cd ..
 
-sed -i '310 s/^setting =.*/setting = $(st)/' '$PATH_TO_FASTNLP/core/dataset.py'
+sed -i '310 s/^setting =.*/setting = $(st)/' $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/dataset.py
 
 cp SIGHUM_embeds/* .
 
