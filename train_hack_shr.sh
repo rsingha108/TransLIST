e8cbfb12e16fa2b47159f21418f28f66ca6d9872
rsingha108 <97200032+rsingha108@users.noreply.github.com>
2022-10-16 05:29:10 -0700

    Update train_hack_shr.sh

--- train_hack_shr.sh
+++ train_hack_shr.sh
@@ -7,10 +7,9 @@ cd sktWS
 cp hackathon/* .
 cd ..
 
-sed -i '310 s/^setting =.*/setting = $(st)/' $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/dataset.py
+sed -i "310 s/^setting =.*/setting = '${st}'/" $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/dataset.py
 
 cp Hackathon_data/embeds/* .
 
-cd V0
 
 
