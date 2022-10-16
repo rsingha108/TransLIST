b1cdbd7ee7f481aeff237f1cb1d5f41731a85e99
rsingha108 <97200032+rsingha108@users.noreply.github.com>
2022-10-16 05:30:15 -0700

    Update train_sighum_shr.sh

--- train_sighum_shr.sh
+++ train_sighum_shr.sh
@@ -7,10 +7,9 @@ cd sktWS
 cp SIGHUM/* .
 cd ..
 
-sed -i '310 s/^setting =.*/setting = $(st)/' $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/dataset.py
+sed -i "310 s/^setting =.*/setting = '${st}'/" $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/dataset.py
 
 cp SIGHUM_embeds/* .
 
-cd V0
 
 
