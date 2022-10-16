5508bff1fa0b67d5194f61dc33c241d0f3867f93
rsingha108 <97200032+rsingha108@users.noreply.github.com>
2022-10-16 05:17:33 -0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,10 +2,6 @@
 
 PATH_TO_ENVS="$1"
 
-conda env create -f tlat0.yml
-
-conda activate tlat0
-
 cp fastnlp-copy/core/tester.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/
 
 cp fastnlp-copy/core/dataset.py $PATH_TO_ENVS/tlat0/lib/python3.7/site-packages/fastNLP/core/
